8a089da7cb5412ba65cb510a00e4eeb3b47dc06c
vastopol <classicunix@gmail.com>
2018-12-24 22:38:09 -0800

    improve setup.sh manual build part

--- setup.sh
+++ setup.sh
@@ -3,6 +3,12 @@
 # script to build the FPL interpreter and manual
 # this will create bin/ && manual.pdf in current directory
 
+function main()
+{
+    build_interp
+    build_manual
+}
+
 #----------------------------------------
 
 # build interpreter
@@ -15,25 +21,29 @@ function build_interp()
     cd ..
 }
 
-# build manual from docs
+# build pdf manual from postscript
 function build_manual()
 {
+    # code to pdf
+    a2ps -R -1 code/main.cpp -o main.ps
     a2ps -R -1 code/header/* -o hed.ps
     a2ps -R -1 code/source/* -o src.ps
-    a2ps -R -1 code/main.cpp -o main.ps
-    a2ps -R -1 DOCS/* -o docs.ps
-
-    ps2pdf docs.ps docs.pdf
     ps2pdf main.ps main.pdf
     ps2pdf hed.ps  hed.pdf
     ps2pdf src.ps  src.pdf
 
+    # documents to pdf (only the numbered ones matter)
+    a2ps -R -1 docs/[0-9]* -o docs.ps
+    ps2pdf docs.ps docs.pdf
+
+    # join pdfs into manual
     pdfunite docs.pdf main.pdf hed.pdf src.pdf manual.pdf
 
+    # cleanup extras
     rm *.ps docs.pdf main.pdf hed.pdf src.pdf
 }
 
 #----------------------------------------
 
-build_interp
-build_manual
+# call script driver
+main
